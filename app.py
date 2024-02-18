d0b54207b6ebc83123ba896e6a7fb3094b4e3372
cbrit0 <cristobal.brito.p@gmail.com>
2024-02-18 20:21:31 -0300

    phone list

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return jsonify(message="Hello, World!")
+    return jsonify(
+        phone_names=phone_names,
+        final_prices=final_prices)
 
+response = requests.get("https://catalogo.movistar.cl/tienda/celulares/apple")
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+phones = [phone for phone in soup.find_all("div", class_="product-item-info")]
+phone_names = [phone.find("h2").text.strip() for phone in phones]
+final_prices = [phone.find("span", class_="divCon-plan-txt-valor-sinLabel").text.strip() for phone in phones]
 
 CORS(app)
 
